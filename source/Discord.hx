cae843404b636495d9ec9c2fb083b756f955eac3
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-04-29 12:28:40 +0800

    Update Discord RPC

--- source/Discord.hx
+++ source/Discord.hx
@@ -11,7 +11,7 @@ class DiscordClient
 	{
 		trace("Discord Client starting...");
 		DiscordRpc.start({
-			clientID: "863222024192262205",
+			clientID: "969454664137445436",
 			onReady: onReady,
 			onError: onError,
 			onDisconnected: onDisconnected
