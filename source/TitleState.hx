2bdf744b48d0056ef4d2b8992f6330b4d59a6d65
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2022-05-06 20:07:04 +0800

    Ok nvm, don't remove the psyka easter code
    it broke the whole damn engine

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -177,19 +177,19 @@ class TitleState extends MusicBeatState
 		// logoBl.color = FlxColor.BLACK;
 
 		swagShader = new ColorSwap();
-		// if(!FlxG.save.data.psykaEasterEgg || !easterEggEnabled) {
-		// 	gfDance = new FlxSprite(FlxG.width * 0.4, FlxG.height * 0.07);
-		// 	gfDance.frames = Paths.getSparrowAtlas('gfDanceTitle');
-		// 	gfDance.animation.addByIndices('danceLeft', 'gfDance', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
-		// 	gfDance.animation.addByIndices('danceRight', 'gfDance', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
-		// }
-		// else //Psyka easter egg
-		// {
-		// 	gfDance = new FlxSprite(FlxG.width * 0.4, FlxG.height * 0.04);
-		// 	gfDance.frames = Paths.getSparrowAtlas('psykaDanceTitle');
-		// 	gfDance.animation.addByIndices('danceLeft', 'psykaDance', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
-		// 	gfDance.animation.addByIndices('danceRight', 'psykaDance', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
-		// }
+		if(!FlxG.save.data.psykaEasterEgg || !easterEggEnabled) {
+			gfDance = new FlxSprite(FlxG.width * 0.4, FlxG.height * 0.07);
+			gfDance.frames = Paths.getSparrowAtlas('gfDanceTitle');
+			gfDance.animation.addByIndices('danceLeft', 'gfDance', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
+			gfDance.animation.addByIndices('danceRight', 'gfDance', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
+		}
+		else //Psyka easter egg
+		{
+			gfDance = new FlxSprite(FlxG.width * 0.4, FlxG.height * 0.04);
+			gfDance.frames = Paths.getSparrowAtlas('psykaDanceTitle');
+			gfDance.animation.addByIndices('danceLeft', 'psykaDance', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
+			gfDance.animation.addByIndices('danceRight', 'psykaDance', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
+		}
 		gfDance.antialiasing = ClientPrefs.globalAntialiasing;
 		add(gfDance);
 		gfDance.shader = swagShader.shader;
@@ -320,7 +320,6 @@ class TitleState extends MusicBeatState
 				});
 				// FlxG.sound.play(Paths.music('titleShoot'), 0.7);
 			}
-			/*
 			else if(easterEggEnabled)
 			{
 				var finalKey:FlxKey = FlxG.keys.firstJustPressed();
@@ -364,7 +363,6 @@ class TitleState extends MusicBeatState
 					}
 				}
 			}
-			*/
 		}
 
 		if (pressedEnter && !skippedIntro)
