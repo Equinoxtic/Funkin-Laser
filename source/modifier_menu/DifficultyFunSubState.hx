5c0206a0abb3a5ce5bf8214adf57eaf864de523b
Vertic <thisistherealequinoctic@gmail.com>
2022-07-09 18:20:47 +0800

    Improved mod menu system part 3

--- source/modifier_menu/DifficultyFunSubState.hx
+++ source/modifier_menu/DifficultyFunSubState.hx
@@ -29,44 +29,18 @@ import Controls;
 
 using StringTools;
 
-class DifficultyReductionSubState extends BaseModOptionsMenu
+class DifficultyFunSubState extends BaseModOptionsMenu
 {
 	public function new()
 	{
-		title = "Difficulty Reduction";
-		rpcTitle = "Modifier Menu - Difficulty Reduction";
+		title = "Fun and Deadly";
+		rpcTitle = "Modifier Menu - Fun and Deadly";
 		getOptions();
 		super();
 	}
 
 	function getOptions()
 	{
-		var option:ModOption = new ModOption("No Fail",
-			"Pretty self-explanatory.",
-			'noFail',
-			'bool',
-			false);
-		addOption(option);
-
-		var option:ModOption = new ModOption("Modcharts Disabled",
-			"Enable this to disable modcharts.",
-			'disabledModcharts',
-			'bool',
-			false);
-		addOption(option);
-
-		var option:ModOption = new ModOption("Botplay",
-			"Enabling this will let the game play for you.",
-			'botplay',
-			'bool',
-			false);
-		addOption(option);
-
-		var option:ModOption = new ModOption("Practice Mode",
-			"Enabling this will let you basically have no fail.\nBUT you won't get any score from it.",
-			'practice',
-			'bool',
-			false);
-		addOption(option);
+		
 	}
 }
