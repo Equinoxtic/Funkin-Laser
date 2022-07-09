927be21dc1e25fb5fa16fbfd7b35b41a44945f1d
Vertic <thisistherealequinoctic@gmail.com>
2022-07-09 17:22:42 +0800

    Improved mod menu system part 1

--- source/modifier_menu/DifficultyIncreaseSubState.hx
+++ source/modifier_menu/DifficultyIncreaseSubState.hx
@@ -23,331 +23,56 @@ import flixel.tweens.FlxTween;
 import flixel.util.FlxTimer;
 import flixel.input.keyboard.FlxKey;
 import flixel.graphics.FlxGraphic;
-import checkbox_utils.CheckboxThingie;
-import attached_utils.AttachedText;
-import preference_vars.ModifierVars;
-import character_classes.Character;
+import options_classes.ModOption;
+import base_options.BaseModOptionsMenu;
+import Controls;
 
 using StringTools;
 
-class DifficultyIncreaseSubState extends MusicBeatSubstate
+class DifficultyIncreaseSubState extends BaseModOptionsMenu
 {
-	private static var curSelected:Int = 0;
-	static var unselectableOptions:Array<String> = [
-		'MODIFIERS',
-		'MULTIPLIERS'
-	];
-	static var noCheckbox:Array<String> = [
-		'Ghost Note Alpha',
-		'Limited Vision Intensity',
-		'Nausea Intensity'
-	];
-	static var options:Array<String> = [
-		'MODIFIERS',
-		// 'Hi-Fi',
-		'Ghost Notes',
-		'Limited Vision',
-		'Nausea',
-		'Flipped',
-		'MULTIPLIERS',
-		'Ghost Note Alpha',
-		'Limited Vision Intensity',
-		'Nausea Intensity'
-	];
-
-	private var grpOptions:FlxTypedGroup<Alphabet>;
-	private var checkboxArray:Array<CheckboxThingie> = [];
-	private var checkboxNumber:Array<Int> = [];
-	private var grpTexts:FlxTypedGroup<AttachedText>;
-	private var textNumber:Array<Int> = [];
-
-	private var showCharacter:Character = null;
-	private var descText:FlxText;
-
 	public function new()
 	{
+		title = "Difficulty Increase";
+		rpcTitle = "Modifier Menu - Difficulty Increase";
 		super();
-		// avoids lagspikes while scrolling through menus!
-		showCharacter = new Character(840, 170, 'bf', true);
-		showCharacter.setGraphicSize(Std.int(showCharacter.width * 0.8));
-		showCharacter.updateHitbox();
-		showCharacter.dance();
-		add(showCharacter);
-		showCharacter.visible = false;
-
-		grpOptions = new FlxTypedGroup<Alphabet>();
-		add(grpOptions);
-
-		grpTexts = new FlxTypedGroup<AttachedText>();
-		add(grpTexts);
-
-		for (i in 0...options.length)
-		{
-			var isCentered:Bool = unselectableCheck(i);
-			var optionText:Alphabet = new Alphabet(0, 70 * i, options[i], false, false);
-			optionText.isMenuItem = true;
-			if(isCentered) {
-				optionText.screenCenter(X);
-				optionText.forceX = optionText.x;
-			} else {
-				optionText.x += 300;
-				optionText.forceX = 300;
-			}
-			optionText.yMult = 90;
-			optionText.targetY = i;
-			grpOptions.add(optionText);
-
-			if(!isCentered) {
-				var useCheckbox:Bool = true;
-				for (j in 0...noCheckbox.length) {
-					if(options[i] == noCheckbox[j]) {
-						useCheckbox = false;
-						break;
-					}
-				}
-
-				if(useCheckbox) {
-					var checkbox:CheckboxThingie = new CheckboxThingie(optionText.x - 105, optionText.y, false);
-					checkbox.sprTracker = optionText;
-					checkboxArray.push(checkbox);
-					checkboxNumber.push(i);
-					add(checkbox);
-				} else {
-					var valueText:AttachedText = new AttachedText('0', optionText.width + 80);
-					valueText.sprTracker = optionText;
-					grpTexts.add(valueText);
-					textNumber.push(i);
-				}
-			}
-		}
-
-		descText = new FlxText(50, 600, 1180, "", 32);
-		descText.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-		descText.scrollFactor.set();
-		descText.borderSize = 2.4;
-		add(descText);
-
-		for (i in 0...options.length) {
-			if(!unselectableCheck(i)) {
-				curSelected = i;
-				break;
-			}
-		}
-		changeSelection();
-		reloadValues();
 	}
 
-	var nextAccept:Int = 5;
-	var holdTime:Float = 0;
-	override function update(elapsed:Float)
+	function getOptions()
 	{
-		if (controls.UI_UP_P)
-		{
-			changeSelection(-1);
-		}
-		if (controls.UI_DOWN_P)
-		{
-			changeSelection(1);
-		}
+		var option:ModOption = new ModOption("Ghost Notes",
+			"If enabled, the notes will become transparent.",
+			'ghostNotes',
+			'bool',
+			false);
+		addOption(option);
 
-		if (controls.BACK) {
-			grpOptions.forEachAlive(function(spr:Alphabet) {
-				spr.alpha = 0;
-			});
-			grpTexts.forEachAlive(function(spr:AttachedText) {
-				spr.alpha = 0;
-			});
-			for (i in 0...checkboxArray.length) {
-				var spr:CheckboxThingie = checkboxArray[i];
-				if(spr != null) {
-					spr.alpha = 0;
-				}
-			}
-			if(showCharacter != null) {
-				showCharacter.alpha = 0;
-			}
-			descText.alpha = 0;
-			close();
-			FlxG.sound.play(Paths.sound('cancelMenu'));
-			#if desktop
-			DiscordClient.changePresence("Modifiers Menu", null);
-			#end
-		}
+		var option:ModOption = new ModOption("Limited Vision",
+			"Enabling this will have a vignette overlaying the HUD.",
+			'limitedVision',
+			'bool',
+			false);
+		addOption(option);
 
-		var usesCheckbox = true;
-		for (i in 0...noCheckbox.length) {
-			if(options[curSelected] == noCheckbox[i]) {
-				usesCheckbox = false;
-				break;
-			}
-		}
+		var option:ModOption = new ModOption("Nausea",
+			"(@...@\")",
+			'nauseous',
+			'bool',
+			false);
+		addOption(option);
 
-		if(usesCheckbox) {
-			if(controls.ACCEPT && nextAccept <= 0) {
-				switch(options[curSelected]) {
-					// case 'Hi-Fi':
-					// 	ModifierVars.hifi = !ModifierVars.hifi;
-					case 'Ghost Notes':
-						ModifierVars.ghostNotes = !ModifierVars.ghostNotes;
-					case 'Limited Vision':
-						ModifierVars.limitedVision = !ModifierVars.limitedVision;
-					case 'Nausea':
-						ModifierVars.nauseous = !ModifierVars.nauseous;
-					case 'Flipped':
-						ModifierVars.flipped = !ModifierVars.flipped;
-				}
-				FlxG.sound.play(Paths.sound('scrollMenu'));
-				reloadValues();
-			}
-		} else {
-			if(controls.UI_LEFT || controls.UI_RIGHT) {
-				var addFloat:Float = controls.UI_LEFT ? -0.01 : 0.01;
-				if(holdTime > 0.5 || controls.UI_LEFT_P || controls.UI_RIGHT_P)
-				switch(options[curSelected]) {
-					/* case 'Fast Song Multiplier':
-						ModifierVars.fastSpeed += addFloat; */
-					case 'Ghost Note Alpha':
-						ModifierVars.ghostNoteAlpha += addFloat;
-					case 'Limited Vision Intensity':
-						ModifierVars.lvIntensity += addFloat;
-					case 'Nausea Intensity':
-						ModifierVars.nauseaIntensity += addFloat;
-					}
-				reloadValues();
+		var option:ModOption = new ModOption("Flipped",
+			"Once enabled, the camera will flip during the gameplay.",
+			'flipped',
+			'bool',
+			false);
+		addOption(option);
 
-				if(holdTime <= 0) FlxG.sound.play(Paths.sound('scrollMenu'));
-				holdTime += elapsed;
-			} else {
-				holdTime = 0;
-			}
-		}
-
-		if(showCharacter != null && showCharacter.animation.curAnim.finished) {
-			showCharacter.dance();
-		}
-
-		if(nextAccept > 0) {
-			nextAccept -= 1;
-		}
-		super.update(elapsed);
-	}
-	
-	function changeSelection(change:Int = 0)
-	{
-		do {
-			curSelected += change;
-			if (curSelected < 0)
-				curSelected = options.length - 1;
-			if (curSelected >= options.length)
-				curSelected = 0;
-		} while(unselectableCheck(curSelected));
-
-		var daText:String = '';
-		switch(options[curSelected]) {
-			/* case 'Hi-Fi':
-				daText = "Make it intense with Hi-Fi songs."; */
-			case 'Ghost Notes':
-				daText = "If checked, the Notes will become more transparent.";
-			case 'Limited Vision':
-				daText = "When enabled, the screen will be much darker.\nEach note hit will also make the screen darker aswell.";
-			case 'Nausea':
-				daText = "Dizzy-ass gameplay if you enable.";
-			case 'Flipped':
-				daText = "Enabling this will flip the gameplay.";
-			/* case 'Fast Song Multiplier':
-				daText = "Set the value of how fast a song should go.\n(Fast Song mod must be enabled)"; */
-			case 'Ghost Note Alpha':
-				daText = "Set the value of how transparent notes should be.\n(Ghost Notes mod must be enabled)";
-			case 'Limited Vision Intensity':
-				daText = "Set how dark the screen should be.\n(Limited Vision mod must be enabled)";
-			case 'Nausea Intensity':
-				daText = "Set how nauseous the screen can get.\n(Nausea mod must be enabled)";
-		}
-		descText.text = daText;
-
-		var bullShit:Int = 0;
-
-		for (item in grpOptions.members) {
-			item.targetY = bullShit - curSelected;
-			bullShit++;
-
-			if(!unselectableCheck(bullShit-1)) {
-				item.alpha = 0.6;
-				if (item.targetY == 0) {
-					item.alpha = 1;
-				}
-
-				for (j in 0...checkboxArray.length) {
-					var tracker:FlxSprite = checkboxArray[j].sprTracker;
-					if(tracker == item) {
-						checkboxArray[j].alpha = item.alpha;
-						break;
-					}
-				}
-			}
-		}
-		for (i in 0...grpTexts.members.length) {
-			var text:AttachedText = grpTexts.members[i];
-			if(text != null) {
-				text.alpha = 0.6;
-				if(textNumber[i] == curSelected) {
-					text.alpha = 1;
-				}
-			}
-		}
-
-		showCharacter.visible = (options[curSelected] == 'Anti-Aliasing');
-		FlxG.sound.play(Paths.sound('scrollMenu'));
-	}
-
-	function reloadValues() {
-		for (i in 0...checkboxArray.length) {
-			var checkbox:CheckboxThingie = checkboxArray[i];
-			if(checkbox != null) {
-				var daValue:Bool = false;
-				switch(options[checkboxNumber[i]]) {
-					// case 'Hi-Fi':
-					// 	daValue = ModifierVars.hifi;
-					case 'Ghost Notes':
-						daValue = ModifierVars.ghostNotes;
-					case 'Limited Vision':
-						daValue = ModifierVars.limitedVision;
-					case 'Nausea':
-						daValue = ModifierVars.nauseous;
-					case 'Flipped':
-						daValue = ModifierVars.flipped;
-				}
-				checkbox.daValue = daValue;
-			}
-		}
-		for (i in 0...grpTexts.members.length) {
-			var text:AttachedText = grpTexts.members[i];
-			if(text != null) {
-				var daText:String = '';
-				switch(options[textNumber[i]]) {
-					/* case 'Fast Song Multiplier':
-						daText = '' + ModifierVars.fastSpeed; */
-					case 'Ghost Note Alpha':
-						daText = '' + ModifierVars.ghostNoteAlpha;
-					case 'Limited Vision Intensity':
-						daText = '' + ModifierVars.lvIntensity;
-					case 'Nausea Intensity':
-						daText = '' + ModifierVars.nauseaIntensity;
-				}
-				var lastTracker:FlxSprite = text.sprTracker;
-				text.sprTracker = null;
-				text.changeText(daText);
-				text.sprTracker = lastTracker;
-			}
-		}
-	}
-
-	private function unselectableCheck(num:Int):Bool {
-		for (i in 0...unselectableOptions.length) {
-			if(options[num] == unselectableOptions[i]) {
-				return true;
-			}
-		}
-		return options[num] == '';
+		var option:ModOption = new ModOption("Health Drain",
+			"Once enabled, the health starts gradually going down.\n(Can be configured.)",
+			'healthDrain',
+			'bool',
+			false);
+		addOption(option);
 	}
 }
--- source/modifier_menu/FreeplayModMenu.hx
+++ source/modifier_menu/FreeplayModMenu.hx
@@ -22,343 +22,99 @@ import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxTimer;
 import flixel.input.keyboard.FlxKey;
-import checkbox_utils.CheckboxThingie;
-import attached_utils.AttachedText;
-import preference_vars.ModifierVars;
-import character_classes.Character;
 import flixel.graphics.FlxGraphic;
+import options_classes.ModOption;
+import base_options.BaseModOptionsMenu;
+import Controls;
 
 using StringTools;
 
-class FreeplayModMenu extends MusicBeatSubstate
+class FreeplayModMenu extends BaseModOptionsMenu
 {
-	private static var curSelected:Int = 0;
-	static var unselectableOptions:Array<String> = [
-		'GAMEMODES',
-		'MODS',
-		'MULTIPLIERS'
-	];
-	static var noCheckbox:Array<String> = [
-		'Health Drain Amount',
-		'Song Speed'
-	];
-	
-	static var options:Array<String> = [
-		'GAMEMODES',
-		'Pussy Mode',
-		'Hardcore Mode',
-		'MODS',
-		'Botplay',
-		'Practice Mode',
-		'No Fail',
-		'Health Drain',
-		'MULTIPLIERS',
-		'Health Drain Amount',
-		'Song Speed'
-	];
-
-	private var grpOptions:FlxTypedGroup<Alphabet>;
-	private var checkboxArray:Array<CheckboxThingie> = [];
-	private var checkboxNumber:Array<Int> = [];
-	private var grpTexts:FlxTypedGroup<AttachedText>;
-	private var textNumber:Array<Int> = [];
-
-	private var showCharacter:Character = null;
-	private var descText:FlxText;
-	var canSelectShit:Bool = false;
-
 	public function new()
 	{
-		super();
+		title = "Freeplay Modifier Menu";
+		rpcTitle = "Freeplay - Modifier Menu";
 
-		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width * 2, FlxG.height * 2, FlxColor.BLACK);
-		bg.scrollFactor.set();
+		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.height, FlxG.width, FlxColor.BLACK);
+		bg.setGraphicSize(Std.int(bg.width * 1.1));
 		bg.screenCenter();
-		bg.alpha = 0;
+		bg.alpha = 0.75;
 		add(bg);
-		
-		// avoids lagspikes while scrolling through menus!
-		showCharacter = new Character(840, 170, 'bf', true);
-		showCharacter.setGraphicSize(Std.int(showCharacter.width * 0.8));
-		showCharacter.updateHitbox();
-		showCharacter.dance();
-		add(showCharacter);
-		showCharacter.visible = false;
 
-		grpOptions = new FlxTypedGroup<Alphabet>();
-		add(grpOptions);
+		getOptions();
 
-		grpTexts = new FlxTypedGroup<AttachedText>();
-		add(grpTexts);
-
-		for (i in 0...options.length)
-		{
-			var isCentered:Bool = unselectableCheck(i);
-			var optionText:Alphabet = new Alphabet(0, 70 * i, options[i], false, false);
-			optionText.isMenuItem = true;
-			if(isCentered) {
-				optionText.screenCenter(X);
-				optionText.forceX = optionText.x;
-			} else {
-				optionText.x += 300;
-				optionText.forceX = 300;
-			}
-			optionText.yMult = 90;
-			optionText.targetY = i;
-			grpOptions.add(optionText);
-
-			if(!isCentered) {
-				var useCheckbox:Bool = true;
-				for (j in 0...noCheckbox.length) {
-					if(options[i] == noCheckbox[j]) {
-						useCheckbox = false;
-						break;
-					}
-				}
-
-				if(useCheckbox) {
-					var checkbox:CheckboxThingie = new CheckboxThingie(optionText.x - 105, optionText.y, false);
-					checkbox.sprTracker = optionText;
-					checkboxArray.push(checkbox);
-					checkboxNumber.push(i);
-					add(checkbox);
-				} else {
-					var valueText:AttachedText = new AttachedText('0', optionText.width + 80);
-					valueText.sprTracker = optionText;
-					grpTexts.add(valueText);
-					textNumber.push(i);
-				}
-			}
-		}
-
-		descText = new FlxText(50, 600, 1180, "", 26);
-		descText.setFormat(Paths.font("vcr.ttf"), 26, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
-		descText.scrollFactor.set();
-		descText.borderSize = 2.4;
-		add(descText);
-
-		for (i in 0...options.length) {
-			if(!unselectableCheck(i)) {
-				curSelected = i;
-				break;
-			}
-		}
-
-		FlxTween.tween(bg, {alpha: 0.5}, 0.65, {ease: FlxEase.quartInOut, onComplete: function(twn:FlxTween) {
-			canSelectShit = true;
-		}});
-
-		changeSelection();
-		reloadValues();
+		super();
 	}
 
-	var nextAccept:Int = 5;
-	var holdTime:Float = 0;
-	override function update(elapsed:Float)
+	function getOptions()
 	{
-		if (canSelectShit) {
-			if (controls.UI_UP_P)
-			{
-				changeSelection(-1);
-			}
-			if (controls.UI_DOWN_P)
-			{
-				changeSelection(1);
-			}
+		var option:ModOption = new ModOption("Pussy Mode",
+			"When enabled, the song will slow down\nMaking it easier for you to comprehend pressing notes.",
+			'pussyMode',
+			'bool',
+			false);
+		addOption(option);
 
-			if (controls.BACK) {
-				grpOptions.forEachAlive(function(spr:Alphabet) {
-					spr.alpha = 0;
-				});
-				grpTexts.forEachAlive(function(spr:AttachedText) {
-					spr.alpha = 0;
-				});
-				for (i in 0...checkboxArray.length) {
-					var spr:CheckboxThingie = checkboxArray[i];
-					if(spr != null) {
-						spr.alpha = 0;
-					}
-				}
-				if(showCharacter != null) {
-					showCharacter.alpha = 0;
-				}
-				descText.alpha = 0;
-				close();
-				FlxG.sound.play(Paths.sound('cancelMenu'));
-				#if desktop
-				DiscordClient.changePresence("Modifiers Menu", null);
-				#end
-			}
+		var option:ModOption = new ModOption("Hardcore Mode",
+			"When enabled, the song will speed up\nand there'd also be a modchart topping all of it off.",
+			'hardcoreMode',
+			'bool',
+			false);
+		addOption(option);
 
-			var usesCheckbox = true;
-			for (i in 0...noCheckbox.length) {
-				if(options[curSelected] == noCheckbox[i]) {
-					usesCheckbox = false;
-					break;
-				}
-			}
+		var option:ModOption = new ModOption("Botplay",
+			"Enabling this will let the game play for you.",
+			'botplay',
+			'bool',
+			false);
+		addOption(option);
 
-			if(usesCheckbox) {
-				if(controls.ACCEPT && nextAccept <= 0) {
-					switch(options[curSelected]) {
-						case 'Pussy Mode':
-							ModifierVars.pussyMode = !ModifierVars.pussyMode;
-						case 'Hardcore Mode':
-							ModifierVars.hardcoreMode = !ModifierVars.hardcoreMode;
-						case 'Botplay':
-							ModifierVars.botplay = !ModifierVars.botplay;
-						case 'Practice Mode':
-							ModifierVars.practice = !ModifierVars.practice;
-						case 'No Fail':
-							ModifierVars.noFail = !ModifierVars.noFail;
-						case 'Health Drain':
-							ModifierVars.healthDrain = !ModifierVars.healthDrain;
-					}
-					FlxG.sound.play(Paths.sound('scrollMenu'));
-					reloadValues();
-				}
-			} else {
-				if(controls.UI_LEFT || controls.UI_RIGHT) {
-					var addFloat:Float = controls.UI_LEFT ? -0.1 : 0.1;
-					if(holdTime > 0.5 || controls.UI_LEFT_P || controls.UI_RIGHT_P)
-					switch(options[curSelected]) {
-						case 'Song Speed':
-							ModifierVars.songSpeed += addFloat;
-							if (ModifierVars.songSpeed < 0) ModifierVars.songSpeed = 0;
-							if (ModifierVars.songSpeed > 3) ModifierVars.songSpeed = 3;
-						case 'Health Drain Amount':
-							ModifierVars.healthDrainAmount += addFloat;
-							if (ModifierVars.healthDrainAmount < 0) ModifierVars.healthDrainAmount = 0;
-							if (ModifierVars.healthDrainAmount > 5) ModifierVars.healthDrainAmount = 5;
-					}
-					reloadValues();
+		var option:ModOption = new ModOption("Practice Mode",
+			"Enabling this will let you basically have no fail.\nBUT you won't get any score from it.",
+			'practice',
+			'bool',
+			false);
+		addOption(option);
 
-					if(holdTime <= 0) FlxG.sound.play(Paths.sound('scrollMenu'));
-					holdTime += elapsed;
-				} else {
-					holdTime = 0;
-				}
-			}
-		}
+		var option:ModOption = new ModOption("No Fail",
+			"Pretty self-explanatory.",
+			'noFail',
+			'bool',
+			false);
+		addOption(option);
 
-		if(showCharacter != null && showCharacter.animation.curAnim.finished) {
-			showCharacter.dance();
-		}
+		var option:ModOption = new ModOption("Health Drain",
+			"Once enabled, the health starts gradually going down.\n(Can be configured.)",
+			'healthDrain',
+			'bool',
+			false);
+		addOption(option);
 
-		if(nextAccept > 0) {
-			nextAccept -= 1;
-		}
-		super.update(elapsed);
-	}
-	
-	function changeSelection(change:Int = 0)
-	{
-		do {
-			curSelected += change;
-			if (curSelected < 0)
-				curSelected = options.length - 1;
-			if (curSelected >= options.length)
-				curSelected = 0;
-		} while(unselectableCheck(curSelected));
+		var option:ModOption = new ModOption("Health Drain Amount",
+			"Set the multiplier for Health Drain.",
+			'healthDrainAmount',
+			'percent',
+			0.1);
+		addOption(option);
+		option.displayFormat = '%v';
+		option.scrollSpeed = 15;
+		option.minValue = 0.0;
+		option.maxValue = 5;
+		option.changeValue = 0.1;
+		option.decimals = 1;
 
-		var daText:String = '';
-		switch(options[curSelected]) {
-			case 'Pussy Mode':
-				daText = "If checked, the song will be slowed down.";
-			case 'Hardcore Mode':
-				daText = "If checked, the song will be faster, and a hard modchart plays.\n(You can make the song faster with Song Speed)";
-			case 'Botplay':
-				daText = "Enabling this would enable \"Botplay\" mode, which would let the game play by itself.";
-			case  'Practice Mode':
-				daText = "Enabling this would enable \"Practice\" mode.\nThis mode will grant you no failure, but a penalty once your HP drops to 0.";
-			case 'No Fail':
-				daText = "Enabling this will make you invincible.";
-			case 'Song Speed':
-				daText = "Set how fast the song should go.\n(Going above 0 will speed up the song)";
-		}
-		descText.text = daText;
-
-		var bullShit:Int = 0;
-
-		for (item in grpOptions.members) {
-			item.targetY = bullShit - curSelected;
-			bullShit++;
-
-			if(!unselectableCheck(bullShit-1)) {
-				item.alpha = 0.6;
-				if (item.targetY == 0) {
-					item.alpha = 1;
-				}
-
-				for (j in 0...checkboxArray.length) {
-					var tracker:FlxSprite = checkboxArray[j].sprTracker;
-					if(tracker == item) {
-						checkboxArray[j].alpha = item.alpha;
-						break;
-					}
-				}
-			}
-		}
-		for (i in 0...grpTexts.members.length) {
-			var text:AttachedText = grpTexts.members[i];
-			if(text != null) {
-				text.alpha = 0.6;
-				if(textNumber[i] == curSelected) {
-					text.alpha = 1;
-				}
-			}
-		}
-
-		showCharacter.visible = (options[curSelected] == 'Anti-Aliasing');
-		FlxG.sound.play(Paths.sound('scrollMenu'));
-	}
-
-	function reloadValues() {
-		for (i in 0...checkboxArray.length) {
-			var checkbox:CheckboxThingie = checkboxArray[i];
-			if(checkbox != null) {
-				var daValue:Bool = false;
-				switch(options[checkboxNumber[i]]) {
-					case 'Pussy Mode':
-						daValue = ModifierVars.pussyMode;
-					case 'Hardcore Mode':
-						daValue = ModifierVars.hardcoreMode;
-					case 'Botplay':
-						daValue = ModifierVars.botplay;
-					case 'Practice Mode':
-						daValue = ModifierVars.practice;
-					case 'No Fail':
-						daValue = ModifierVars.noFail;
-					case 'Health Drain':
-						daValue = ModifierVars.healthDrain;
-				}
-				checkbox.daValue = daValue;
-			}
-		}
-		for (i in 0...grpTexts.members.length) {
-			var text:AttachedText = grpTexts.members[i];
-			if(text != null) {
-				var daText:String = '';
-				switch(options[textNumber[i]]) {
-					case 'Song Speed':
-						daText = '' + ModifierVars.songSpeed;
-					case 'Health Drain Amount':
-						daText = '' + ModifierVars.healthDrainAmount;
-				}
-				var lastTracker:FlxSprite = text.sprTracker;
-				text.sprTracker = null;
-				text.changeText(daText);
-				text.sprTracker = lastTracker;
-			}
-		}
-	}
-
-	private function unselectableCheck(num:Int):Bool {
-		for (i in 0...unselectableOptions.length) {
-			if(options[num] == unselectableOptions[i]) {
-				return true;
-			}
-		}
-		return options[num] == '';
+		var option:ModOption = new ModOption("Song Speed Multiplier",
+			"Set how fast the song should be.",
+			'songSpeed',
+			'percent',
+			0);
+		addOption(option);
+		option.displayFormat = '%v';
+		option.scrollSpeed = 15;
+		option.minValue = 0;
+		option.maxValue = 3.5;
+		option.changeValue = 0.1;
+		option.decimals = 1;
 	}
 }
