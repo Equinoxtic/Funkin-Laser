6914848dab70e8988eadc3a5cf2253bcc57881ad
Vertic <thisistherealequinoctic@gmail.com>
2022-07-11 23:31:37 +0800

    Create CameraSettings.hx

---
+++ source/preferences_menu/CameraSettings.hx
@@ -0,0 +1,32 @@
+package preferences_menu;
+
+#if desktop
+import Discord.DiscordClient;
+#end
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.math.FlxMath;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.utils.Assets;
+import flixel.FlxSubState;
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.util.FlxSave;
+import haxe.Json;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxTimer;
+import flixel.input.keyboard.FlxKey;
+import flixel.graphics.FlxGraphic;
+import options_classes.Option;
+import base_options.BaseOptionsMenu;
+import Controls;
+
+using StringTools;
+
+// TODO: Finish this code idk
