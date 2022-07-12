244492a9edc6fd2d0e420313e609e3a760714da6
Vertic <thisistherealequinoctic@gmail.com>
2022-07-12 08:33:41 +0800

    Add options for Gameplay Settings

--- source/preferences_menu/GameplaySettings.hx
+++ source/preferences_menu/GameplaySettings.hx
@@ -29,4 +29,51 @@ import Controls;
 
 using StringTools;
 
-// TODO: Finish this code
+class GameplaySettings extends BaseOptionsMenu
+{
+	public function new()
+	{
+		title = "Gameplay Settings";
+		rpcTitle = "Preferences - Gameplay Settings";
+		getOptions();
+		super();
+	}
+
+	function getOptions()
+	{
+		var option:Option = new Option("Middlescroll",
+			"If checked, hides Opponent's notes and your notes get centered.",
+			'middleScroll',
+			'bool',
+			false);
+		addOption(option);
+
+		var option:Option = new Option("Downscroll",
+			"If checked, notes go Down instead of Up, simple enough.",
+			'downScroll',
+			'bool',
+			false);
+		addOption(option);
+
+		var option:Option = new Option("Note Splashes",
+			"If unchecked, hitting \"Sick!\" notes won't show particles.",
+			'noteSplashes',
+			'bool',
+			true);
+		addOption(option);
+
+		var option:Option = new Option("Show Judgement Data",
+			"Hides the statistics of gained judgements when unchecked.",
+			'showJudgementData',
+			'bool',
+			true);
+		addOption(option);
+
+		var option:Option = new Option("Advanced Scoring System",
+			"If checked, you will enable the Advanced Scoring System.\n(Score Multiplier + Extra HUD for gained score & combo)",
+			'advancedScoring',
+			'bool',
+			false);
+		addOption(option);
+	}
+}
